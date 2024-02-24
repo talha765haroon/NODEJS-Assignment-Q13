f239aa640cead06fb9eb0230497dbd65b8300936
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:43:58 +0500

    Add files via upload

---
+++ q13.js
@@ -0,0 +1,7 @@
+"use strict";
+/*Ques # 13:
+Your Own Array:Think of your favourite mode of transportation,such as a motorcycle,or  a car and make a list that stores several examples.Use your list to print a series of statement about these items,such as"I would like to own a Honda motorcycle */
+let vehichles = ["Honda motarcycle", "Hilux", "Audi", "Honda city"];
+vehichles.map((items) => {
+    console.log(`I would like to own a ${items}`);
+});
---
+++ q13.ts
@@ -0,0 +1,9 @@
+/*Ques # 13:
+Your Own Array:Think of your favourite mode of transportation,such as a motorcycle,or  a car and make a list that stores several examples.Use your list to print a series of statement about these items,such as"I would like to own a Honda motorcycle */
+
+let vehichles: string[] = ["Honda motorcycle",  "Hilux",  "Audi", "suzuki", "Honda city"];
+
+vehichles.map((items) =>{
+  console.log(`I would like to own a ${items}`)
+});
+    
